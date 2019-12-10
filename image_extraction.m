fc1cacd90f033ae44ca75f7bed2a0c252995a091
vijayg15 <mech.vijayg@gmail.com>
2019-12-11 00:39:21 +0530

    Update image_extraction.m

--- image_extraction.m
+++ image_extraction.m
@@ -2,9 +2,9 @@ clc;
 clear all;
 close all;
 
-Video_data = VideoReader('D:/OMAXE/2018_0506_012841_014.mp4');
+Video_data = VideoReader('./video.mp4');
 get(Video_data)
-for img = 8379:Video_data.NumberOfFrames;
+for img = 0:Video_data.NumberOfFrames;
     filename = strcat('frame',num2str(img),'.jpg');
     VidFrames = read(Video_data, img);
     imwrite(VidFrames, filename);
